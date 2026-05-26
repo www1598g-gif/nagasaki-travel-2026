7d8f6f09c4a18e3d5b97558f8bdcf7411ed9ae94
www1598g-gif <www1598g@gmail.com>
2026-05-26 22:50:46 +0800

    start

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,17 @@ import { getDatabase } from "firebase/database";
 // 👇 下面這一串要換成你剛剛在 Firebase 網站上申請的喔！
 // Your web app's Firebase configuration
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCYjJymGsrS_DWnndUR6MY2KUSmjXE-N7A",
-  authDomain: "chiangmai-2026.firebaseapp.com",
-  databaseURL: "https://chiangmai-2026-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chiangmai-2026",
-  storageBucket: "chiangmai-2026.firebasestorage.app",
-  messagingSenderId: "515846045286",
-  appId: "1:515846045286:web:dec91f8868d5cdece5b7a7"
+  apiKey: "AIzaSyD83tQLQOKT6Mom6mIBFIIQUWhjZzgIN5Q",
+  authDomain: "nagasaki-2026.firebaseapp.com",
+  databaseURL: "https://nagasaki-2026-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nagasaki-2026",
+  storageBucket: "nagasaki-2026.firebasestorage.app",
+  messagingSenderId: "745050256149",
+  appId: "1:745050256149:web:eb312690e754c61a50ef1c"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
